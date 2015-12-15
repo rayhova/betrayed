0113773229e0d11af4a1b3677cb04b49a5cd939a
unknown <raymond.gothaii@gmail.com>
2015-12-15 13:27:47 -0500

    login style and new mobile nav

--- functions.php
+++ functions.php
@@ -264,3 +264,29 @@ function prefix_insert_after_paragraph( $insertion, $paragraph_id, $content ) {
 	
 	return implode( '', $paragraphs );
 }
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/style-login.css' );
+    wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/style-login.js' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
